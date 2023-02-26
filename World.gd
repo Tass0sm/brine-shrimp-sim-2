cb89cf1d8f26d13e5fc9c97f11257bd0edd0727e
John Mays <25927827+johnmays@users.noreply.github.com>
2023-02-26 11:16:04 -0500

    added popups functionality

--- World.gd
+++ World.gd
@@ -8,9 +8,30 @@ extends Spatial
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$AudioStreamPlayer.play()
+	pause_mode = Node.PAUSE_MODE_PROCESS
+	
+	$vomit_popup.hide()
+	$mating_popup.hide()
+	
+	$ambience.play()
 	pass
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+
+func _process(delta):
+	if !get_tree().paused:
+		var randval = rand_range(0.0,1.0)
+		if randval < (2.0/(60.0*60.0)) and randval > (1.0/(60.0*60.0)):
+			get_tree().paused = true
+			$vomit_popup.show()
+			$fx_vomit3.play()
+		elif randval < (1.0/(60.0*60.0)):
+			get_tree().paused = true
+			$mating_popup.show()
+			$fx_splat1.play()
+
+func _unhandled_input(event):
+	if event is InputEventKey:
+		if event.pressed and event.scancode == KEY_ENTER:
+			$vomit_popup.hide()
+			$mating_popup.hide()
+			get_tree().paused = false

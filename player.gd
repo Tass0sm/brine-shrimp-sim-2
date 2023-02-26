3e2c917a30ef12fd87f2f9b077a8402415c1e694
John Mays <25927827+johnmays@users.noreply.github.com>
2023-02-25 21:22:36 -0500

    added player, random movement

--- player.gd
+++ player.gd
@@ -1,16 +1,41 @@
-extends Spatial
+extends KinematicBody
 
+# var rng = RandomNumberGenerator.new()
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+var base_speed = 0.8
+var base_swim_freq = 0.65
+var base_direction_delta = 0.01
 
+var time : float
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+var direction = Vector3.ZERO
+var magnitude : float
+var velocity = Vector3.ZERO	
 
+func initialize():
+	# time = 0
+	# magnitude = base_speed*((cos(2*PI*base_swim_freq*time)) + 1.5)
+	var x = rand_range(-1.0,1.0)
+	var y = rand_range(-1.0,1.0)
+	var z = rand_range(-1.0,1.0)
+	direction = Vector3(x,y,z)
+	direction = direction.normalized()
+	
+func _physics_process(_delta):
+	# warning-ignore:return_value_discarded
+	time += _delta
+	direction = get_direction()
+	magnitude = get_forward_speed()
+	velocity = magnitude*direction
+	move_and_slide(velocity)
+	rotation = direction
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-# func _process(delta):
-#	pass
+func get_forward_speed():
+	return base_speed*((cos(2*PI*base_swim_freq*time)) + 1.5)
+
+func get_direction():
+	var dx = base_direction_delta*sin(0.42*time)
+	var dy = base_direction_delta*sin(0.35*time)
+	var dz = base_direction_delta*sin(0.3*time)
+	var temp_direction = direction + Vector3(dx, dy, dz)
+	return temp_direction.normalized()
